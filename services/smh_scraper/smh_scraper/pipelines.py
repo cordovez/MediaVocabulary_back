c9310e0cfd5d5dc0443736b55e5b761ff651235a
JC CorMan <cordovez@el-cordovez.com>
2023-06-07 17:07:09 +0200

    smh scrape to Mongodb

--- services/smh_scraper/smh_scraper/pipelines.py
+++ services/smh_scraper/smh_scraper/pipelines.py
@@ -1,13 +1,37 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+import sys
+from .items import SmhScraperItem
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# explanation of the following code at: 
+# https://www.mongodb.com/basics/how-to-use-mongodb-to-store-scraped-data 
+class MongoDBPipeline:
+    
+    collection = 'sydney_morning_herald'
+    
+    def __init__(self, mongodb_uri, mongodb_db):
+        self.mongodb_uri = mongodb_uri
+        self.mongodb_db = mongodb_db
+        if not self.mongodb_uri: 
+            sys.exit("You need to provide a Connection String")
 
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongodb_uri=crawler.settings.get('MONGODB_URI'),
+            mongodb_db=crawler.settings.get('MONGODB_DATABASE', 'items')
+        )
+    
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongodb_uri)
+        self.db = self.client[self.mongodb_db]
+        # start with a clean database
+        self.db[self.collection].delete_many({})
 
-class SmhScraperPipeline:
+    def close_spider(self, spider):
+        self.client.close()
+        
     def process_item(self, item, spider):
+        data = dict(SmhScraperItem(item))
+        self.db[self.collection].insert_one(data)
         return item
--- services/smh_scraper/smh_scraper/settings.py
+++ services/smh_scraper/smh_scraper/settings.py
@@ -62,10 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "smh_scraper.pipelines.SmhScraperPipeline": 300,
-#}
-
+ITEM_PIPELINES = {
+   "smh_scraper.pipelines.MongoDBPipeline": 500
+}
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
@@ -91,3 +90,8 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+
+
+
