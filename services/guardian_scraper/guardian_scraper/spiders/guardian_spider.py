da95fea220fbfc95682f1e4d7dbf488be78a36e2
JC CorMan <cordovez@el-cordovez.com>
2023-07-27 19:03:40 +0200

    refinements to author name scrape in the Guardian

--- services/guardian_scraper/guardian_scraper/spiders/guardian_spider.py
+++ services/guardian_scraper/guardian_scraper/spiders/guardian_spider.py
@@ -29,16 +29,24 @@ class GuardianSpider(scrapy.Spider):
             uri =  article.css('a').attrib['href']
             url = f'{base_url}{uri}'
 
-            yield scrapy.Request(url, callback=self.parse_article, meta={'url': url})
+
+            yield scrapy.Request(url, callback=self.parse_article, 
+                                 meta={'url': url})
         
     def parse_article(self, response):
             item = GuardianScraperItem()
 
-            item['article_title'] = self.extract_if_available(response.css('div.dcr-0 h1::text').get())  # noqa: E501
-            item['author'] = self.extract_if_available(response.css('div.dcr-0 a::text').get())  # noqa: E501
+            item['article_title'] = self.extract_if_available(response.css('h1::text').get())  # noqa: E501
+
+            author_element = response.css('a[rel="author"]')
+            author_name = author_element.css('::text').get()
+            if author_name is None:
+                item['author'] = "Anonymous"
+            else:
+                item['author'] = author_name            
             item['summary'] = self.extract_if_available(response.css('div.dcr-1yi1cnj p::text').get())  # noqa: E501
-            raw_date = self.extract_if_available(response.css('span.dcr-u0h1qy::text').get()[0:21])  # noqa: E501, F821
             
+            raw_date = self.extract_if_available(response.css('span.dcr-u0h1qy::text').get()[0:21])  # noqa: E501, F821
             if raw_date is not None:
                 # Define the format of the date string based on its structure
                 date_format = "%a %d %b %Y %H.%M"
@@ -48,12 +56,11 @@ class GuardianSpider(scrapy.Spider):
             else:
                 item['date_of_pub'] = None
                     
-            
-                
-                
             text = self.extract_if_available(response.css('div.article-body-commercial-selector.article-body-viewer-selector p ::text').getall())  # noqa: E501
             # text = self.extract_if_available(response.css('div.article-body-commercial-selector.article-body-viewer-selector.dcr-1r94quw p.dcr-94xsh ::text').getall())  # noqa: E501
+           
             item['content'] = ''.join(text).strip() 
+          
             item['url'] = response.meta['url']
             
             yield item
